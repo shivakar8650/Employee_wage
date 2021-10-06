08b7067c151a3e938812a51d7b39d4c96314faa5
shivakar8650 <shivakar.up99@gmail.com>
2021-10-06 15:13:00 +0530

    add attendence check file

---
+++ UC_1.sh
@@ -0,0 +1,10 @@
+
+ispresent=1
+
+random=$((RANDOM%2))
+if(($random==$ispresent))
+then
+	echo "Employee is Present"
+else
+	echo "Employee is Absent"
+fi
--- welcome.sh
+++
@@ -1,3 +0,0 @@
-
-
-echo "Welcome to wage computation program."
