78c64515901ccc6be84b266cf186fc65178a9842
shivakar8650 <shivakar.up99@gmail.com>
2021-10-06 21:23:09 +0530

    add a extra line

--- UC_2.sh
+++ UC_2.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -x
 
 isPresent=1;
-randomCheck=$((RANDOM%2));
+random=$((RANDOM%2));
 
-if [ $isPresent -eq $randomCheck ];
+if [ $isPresent -eq $random ];
 then
 empRatePerHr=20;
   empHrs=8;
+   echo "to checke the merge conflict"
+
   salary=$(($empHrs*$empRatePerHr));
 else
   salary=0;
